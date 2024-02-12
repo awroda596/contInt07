8a5192d23ab1a03deef0d2cf89bb241d2c49feaa
awroda596 <awroda596@gmail.com>
2024-02-12 12:08:34 -0800

    added everything

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std; 
+
+int main(){
+	cout << "Bless the Maker and His water.\n Bless the coming and going of Him.\n May His passing cleanse the world.\n May He keep the world for His people.\n"; 
+	return 0; 
+}
